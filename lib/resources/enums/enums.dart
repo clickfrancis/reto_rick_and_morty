6b6df1ab64fd413750f45ea7bdc9785574bf31a2
clickfrancis <francis.source@gmail.com>
2022-06-18 11:07:28 -0300

    formatting enums v1.0.1

--- lib/resources/enums/enums.dart
+++ lib/resources/enums/enums.dart
@@ -1,36 +1,22 @@
-enum CharacterSpecies {
-  human,
-  alien,
-  empty,
-}
-enum CharacterStatus {
-  alive,
-  unknown,
-  dead,
-  empty,
-}
-enum CharacterGender {
-  male,
-  female,
-  unknown,
-  empty,
-}
+enum CharacterSpecies { human, alien, empty }
+enum CharacterStatus { alive, unknown, dead, empty }
+enum CharacterGender { male, female, unknown, empty }
 
 final characterSpeciesValues = {
   CharacterSpecies.alien: "Alien",
   CharacterSpecies.human: "Human",
-  CharacterSpecies.empty: "",
+  CharacterSpecies.empty: ""
 };
 
 final characterStatusValues = {
   CharacterStatus.alive: "Alive",
   CharacterStatus.dead: "Dead",
-  CharacterStatus.empty: "",
+  CharacterStatus.empty: ""
 };
 
 final characterGenderValues = {
   CharacterGender.female: "Female",
   CharacterGender.male: "Male",
   CharacterGender.unknown: "unknown",
-  CharacterGender.empty: "",
+  CharacterGender.empty: ""
 };
