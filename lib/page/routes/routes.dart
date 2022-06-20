d72ed4a9c75aee99b6c6e5395dd31e8c7692005d
clickfrancis <francis.source@gmail.com>
2022-06-20 01:17:45 -0300

    adding splash

--- lib/page/routes/routes.dart
+++ lib/page/routes/routes.dart
@@ -1,13 +1,16 @@
 import 'package:rick_and_morty_app/page/home_page.dart';
 
-import '../wellcome_page.dart';
+import '../splash.dart';
+import '../welcome_page.dart';
 
 abstract class Routes {
-  static const String welcomePage = "/";
+  static const String splash = "/";
+  static const String welcomePage = "/welcomePage";
   static const String home = "/home";
   static const String characterPage = "/characterPage";
 
   static final routes = {
+    splash: (context) => const Splash(),
     welcomePage: (context) => const WelcomePage(),
     home: (context) => const HomePage(),
   };
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:rick_and_morty_app/page/wellcome_page.dart';
+import 'package:rick_and_morty_app/page/welcome_page.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
